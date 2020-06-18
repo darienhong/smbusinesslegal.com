f0ce891a423e69f54bc2a801d342073f83f1c59c
Darien Hong <darienlake2009@hotmail.com>
2020-06-18 16:12:57 -0400

    edited about page

--- smblegal/src/aboutus.jsx
+++ smblegal/src/aboutus.jsx
@@ -19,6 +19,9 @@ export default class AboutUs extends Component {
               <br></br>
               <br></br>
               <h1 style={{color: "rgb(57, 60, 126)"}}> About Us </h1>
+              <svg height={50} width={700}>
+        <line class="svg-line-1" x1={400} x2={3000} y1={1} y2={1} stroke="black" style={{left: 5000}}/>
+        </svg>
           </div>
 
     
