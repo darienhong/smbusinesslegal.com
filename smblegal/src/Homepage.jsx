d2c193d26255ee00067ff4655ef42ec8bb093f9c
Darien Hong <darienlake2009@hotmail.com>
2020-06-18 10:42:19 -0400

    added animation to homepage text

--- smblegal/src/Homepage.jsx
+++ smblegal/src/Homepage.jsx
@@ -5,6 +5,24 @@ import Button from 'react-bootstrap/Button';
 import Lottie from 'react-lottie';
 import animationData from './assets/animation.json';
 import ModalTest from './components/modal.jsx';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init();
+
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  once: false, // whether animation should happen only once - while scrolling down
+  
+});
 
 
 export default class Homepage extends Component {
@@ -31,6 +49,8 @@ export default class Homepage extends Component {
             height={800}
             width={800}
           />   
+
+          <div data-aos="fade-in"  data-aos-easing="ease-in-out"  data-aos-duration="2000">
            <h1 class="title"> SMB Legal </h1>
            <h2 style={{ textAlign: "center"}}> Small Business Lifecycle Management </h2>
            <br></br>
@@ -50,7 +70,8 @@ export default class Homepage extends Component {
         
              <a href="https://forms.gle/iNUd4My5DKTGHuAz8" target="_blank"> <center> <Button variant="outline-dark" style={{fontFamily: 'WorkSans', fontWeight: 550,
               borderRadius: 0, padding: 10, width: 200}}> JOIN NOW </Button>{' '} </center> </a>
-          
+
+            </div>
            </div>
            <br></br>
            <br></br>
