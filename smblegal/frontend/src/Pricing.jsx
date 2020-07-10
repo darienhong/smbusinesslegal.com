32c9d108c2d370f268f79412a0f97b752cf98842
Avani Aggrwal <aa974@cornell.edu>
2020-07-10 16:52:44 -0700

    adding fixes to board docs

--- smblegal/frontend/src/Pricing.jsx
+++ smblegal/frontend/src/Pricing.jsx
@@ -10,94 +10,94 @@ import Footer from './components/footer.jsx';
 import {
     BrowserRouter as Router,
     Switch,
-    Route, 
+    Route,
     Link
-  } from "react-router-dom";
-  
+} from "react-router-dom";
 
-export default class Pricing extends Component { 
-    constructor(props){
+
+export default class Pricing extends Component {
+    constructor(props) {
         super(props);
     }
     render() {
         return (
-            <div class="full-page"> 
-        <div class="pricing-page">
-            <Navbar />
-            <br></br>
-            <br></br> 
-            <br></br>
-            <br></br>
-            <br></br>
-            <h1 style={{color: "#245CA6"}}> Pricing </h1>
-              <br></br>
-            <div style={{textAlign:"center"}}>
-                <svg height={50} width={400}>
-                    <line class="svg-line-1" x1={1} x2={500} y1={1} y2={1}/>
-                </svg>
-            </div>
-            <br></br>
-            <div class="price-cards">
-                
-        <Link to ="/CreateAccount" style={{textDecoration: "none"}}> 
-             <div class="price-card-1">
-            <Card>
-                <CardContent className="card-2-inside">
-                <h4 class="price-option"> Option 1</h4>
-                <h2 class="price-category">  Starter </h2>
-                <p> Registration </p>
-                <br />
-                <p class="price"> Free </p>
-                <p> INCLUDED: <br /> &bull; 2 Automated Documents per month </p> 
-                <Button size="small">Get Started</Button>
-                </CardContent>
-            </Card>
-            </div> 
-            </Link>
+            <div class="full-page">
+                <div class="pricing-page">
+                    <Navbar />
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <h1 style={{ color: "#245CA6" }}> Pricing </h1>
+                    <br></br>
+                    <div style={{ textAlign: "center" }}>
+                        <svg height={50} width={400}>
+                            <line class="svg-line-1" x1={1} x2={500} y1={1} y2={1} />
+                        </svg>
+                    </div>
+                    <br></br>
+                    <div class="price-cards">
 
-        <Link to ="/CreateAccount" style={{textDecoration: "none"}}> 
-            <div class="price-card-2">
-            <Card>
-                <CardContent className="card-2-inside">
-                <h4 class="price-option"> Option 2 </h4>
-                <h2 class="price-category">  Premium </h2>
-                <p> Subscription </p>
-                <br/>
-                <p class="price"> $10 / Month </p>
-                <p> INCLUDED: <br /> &bull; 15 Automated Documents per month
+                        <Link to="/CreateAccount" style={{ textDecoration: "none" }}>
+                            <div class="price-card-1">
+                                <Card>
+                                    <CardContent className="card-2-inside">
+                                        <h4 class="price-option"> Option 1</h4>
+                                        <h2 class="price-category">  Starter </h2>
+                                        <p> Registration </p>
+                                        <br />
+                                        <p class="price2"> Free </p>
+                                        <p> INCLUDED: <br /> &bull; 2 Automated Documents per month </p>
+                                        <Button size="small">Get Started</Button>
+                                    </CardContent>
+                                </Card>
+                            </div>
+                        </Link>
+
+                        <Link to="/CreateAccount" style={{ textDecoration: "none" }}>
+                            <div class="price-card-2">
+                                <Card>
+                                    <CardContent className="card-2-inside">
+                                        <h4 class="price-option"> Option 2 </h4>
+                                        <h2 class="price-category">  Premium </h2>
+                                        <p> Subscription </p>
+                                        <br />
+                                        <p class="price2"> $10 / Month </p>
+                                        <p> INCLUDED: <br /> &bull; 15 Automated Documents per month
                 <br /> &bull; Free Formation Documents and Guides (only Delaware documents are available now)
                 <br /> &bull; Governance </p>
-                <Button size="small">Get Started</Button>
-                </CardContent>
-            </Card>
-            </div> 
-            </Link>
+                                        <Button size="small">Get Started</Button>
+                                    </CardContent>
+                                </Card>
+                            </div>
+                        </Link>
 
-        <Link to ="/CreateAccount" style={{textDecoration: "none"}}> 
-            <div class="price-card-3">
-            <Card>
-                <CardContent className="card-2-inside">
-                <h4 class="price-option"> Option 3 </h4>
-                <h2 class="price-category">  Formation Documents </h2>
-                <p> Per Unit </p>
-                <br />
-                <p class="price"> $5 / package</p>
-                <p> INCLUDED: <br />  &bull; Automated Formation Documents
+                        <Link to="/CreateAccount" style={{ textDecoration: "none" }}>
+                            <div class="price-card-3">
+                                <Card>
+                                    <CardContent className="card-2-inside">
+                                        <h4 class="price-option"> Option 3 </h4>
+                                        <h2 class="price-category">  Formation Documents </h2>
+                                        <p> Per Unit </p>
+                                        <br />
+                                        <p class="price2"> $5 / package</p>
+                                        <p> INCLUDED: <br />  &bull; Automated Formation Documents
                 <br /> &bull; Formation Resolutions </p>
-                <Button size="small">Get Started</Button>
-                </CardContent>
-            </Card>
-            </div>  
-            </Link>
+                                        <Button size="small">Get Started</Button>
+                                    </CardContent>
+                                </Card>
+                            </div>
+                        </Link>
 
-            </div>
-            <br />
-            <br />
-            <br /> 
+                    </div>
+                    <br />
+                    <br />
+                    <br />
 
+                </div>
+                <Footer />
             </div>
-            <Footer />
-       </div>
 
         );
     }
